50a6153e2b9dee47716e7854df3b9ef52ecfae17
Esben Viskum <viskum@viskum.dk>
2021-04-28 11:14:27 +0200

    Initial Commit

---
+++ Habits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habits
+//
+//  Created by Esben Viskum on 28/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Esben Viskum on 28/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
